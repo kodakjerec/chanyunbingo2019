f0c1d922249df461bcb6f133cc8904288a8ba069
kodakjerec0425 <kodakjerec@gmail.com>
2019-11-27 11:23:05 +0800

    加入axios select table 範例

--- src/views/bingo.vue
+++ src/views/bingo.vue
@@ -1,25 +1,62 @@
 ﻿<template>
-    <div>
+    <div class="main">
         <div>
             <button type="primary" @click="roll">Roll</button>
+            <TableView
+              :headers="columns"
+              :rows="items"
+              :sort="sort1"
+              :pagination="pagination"
+            >
+              <template v-slot:items="{ row }">
+                <td>{{ row.line_id }}</td>
+                <td>{{ row.cell_code }}</td>
+                <td>{{ row.layer_code }}</td>
+                <td>{{ row.cell_x }}</td>
+                <td>{{ row.cell_y }}</td>
+              </template>
+
+              <template v-slot:no-data>
+                <span>No data</span>
+              </template>
+            </TableView>
         </div>
     </div>
 </template>
 <script>
 import axios from 'axios'
+import TableView from './components/table/tableView'
 
 export default {
   name: 'test',
+  components: {
+    TableView
+  },
   data () {
     return {
-      parameters: {}
+      columns: [
+        { 'label': '代號', 'field': 'line_id', 'sortable': true, 'type': 'String' },
+        { 'label': '棟別', 'field': 'cell_code', 'sortable': true, 'type': 'String' },
+        { 'label': '樓層', 'field': 'layer_code', 'sortable': true, 'type': 'String' },
+        { 'label': 'X軸', 'field': 'cell_x', 'sortable': true, 'type': 'Number' }
+      ],
+      items: [],
+      sort1: { field: 'line_id', order: 'asc' },
+      pagination: {
+        itemsPerPage: 10,
+        align: 'center',
+        visualStyle: 'select'
+      }
     }
   },
   methods: {
     roll: function () {
-      axios.post('http://localhost:3000/baseData')
+      axios
+        .post('http://localhost:3000/baseData', {
+          input_parameter: 'A1F1A00A'
+        })
         .then(response => {
-          console.log(response)
+          this.items = response.data
         })
         .catch(error => { // 请求失败处理
           console.log(error)
@@ -28,3 +65,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.main {
+  background-color: white
+}
+</style>
